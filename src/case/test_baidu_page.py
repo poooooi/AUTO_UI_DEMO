106808e46b53af3859216b10f78ea72350277111
pooooi <934435147@qq.com>
2021-02-24 10:18:11 +0800

    feat: 实现yaml文件存放URL配置 1. 导入configReader类 2. 加入URL获取(使用上述方式，进行Config类的实例化及get方法的调用) 3. 使用configReader.py中的DRIVER_PATH来读取chromedriver.exe的目录位置

--- src/case/test_baidu_page.py
+++ src/case/test_baidu_page.py
@@ -1,20 +1,27 @@
-from selenium import webdriver
 import time
 import unittest
 
+from selenium import webdriver
+from utils.configReader import Config,DRIVER_PATH
+
 class LoginTest(unittest.TestCase):
+    URL = Config().get('URL')
+
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome()
+        cls.driver = webdriver.Chrome(executable_path=DRIVER_PATH + '\chromedriver.exe')
 
     def setUp(self):
-        self.driver.get("https://www.baidu.com")
+        self.driver.get(self.URL)
         time.sleep(2)
 
 
     def test_search(self):
         '''搜索的case'''
-        # 1.打开浏览器; 2.找到搜索框，输入"paython"; 3.点击"搜索"按钮; 4.抓取当前页面中所有结果的标题并输出
+        # 1.打开浏览器;
+        # 2.找到搜索框，输入"paython";
+        # 3.点击"搜索"按钮;
+        # 4.抓取当前页面中所有结果的标题并输出
         self.driver.find_element_by_xpath("//input[@id='kw']").send_keys("python自动化")
         self.driver.find_element_by_xpath("//input[@id='su']").click()
         time.sleep(3)
