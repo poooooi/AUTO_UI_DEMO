ac47ff36627fd013c4af544f1798c407cc44dccc
pooooi <934435147@qq.com>
2021-02-27 23:20:27 +0800

    feat: 实现HTML报告的邮件发送； 1. 添加emails.py实现邮件的编写和发送 2. runner.py中增加第三步和第四步

--- runner.py
+++ runner.py
@@ -2,6 +2,7 @@ import os
 import unittest
 import time
 from utils.HTMLTestRunner import HTMLTestRunner
+from utils.emails import Email
 from utils.utilConfig import CASE_PATH, REPORT_PATH
 
 """
@@ -11,26 +12,19 @@ from utils.utilConfig import CASE_PATH, REPORT_PATH
 第三步：获取最新的测试报告
 第四步：发送邮件
 """
- 
- 
-# log = logger(logger='run').getlog()
 
-
-def add_case(caseName = 'case', rule='test*.py'):
+def add_case(rule='test*.py'):
     # 第一步：加载所有测试用例
-    case_path = os.path.join(CASE_PATH, caseName)
-
-    if not os.path.exists(case_path):
-        os.mkdir(case_path)
+    if not os.path.exists(CASE_PATH):
+        os.mkdir(CASE_PATH)
 
     # 定义discover方法的参数(执行全部用例)
-    discover = unittest.defaultTestLoader.discover(case_path, pattern=rule, top_level_dir=None)
+    discover = unittest.defaultTestLoader.discover(CASE_PATH, pattern=rule, top_level_dir=None)
     return discover
 
 
 def run_case(all_case, reportName = 'report'):
     # 第二步：执行所有的用例，并把执行结果写入HTML测试报告中
-
     now = time.strftime('%Y%m%d_%H%M%S_', time.localtime(time.time()))
 
     if not os.path.exists(REPORT_PATH):
@@ -42,9 +36,31 @@ def run_case(all_case, reportName = 'report'):
         runner = HTMLTestRunner(stream=fp, title='web自动化测试报告', description='用例执行情况')
         runner.run(all_case)
 
+def get_report_file(REPORT_PATH):
+    # 第三步：获取最新的测试报告
+    lists = os.listdir(REPORT_PATH)
+    lists.sort(key=lambda fn: os.path.getmtime(os.path.join(REPORT_PATH, fn)))
+
+    print('最新测试报告：' + lists[-1])
+
+    # 找到最新的测试报告文件
+    report_file = os.path.join(REPORT_PATH, lists[-1])
+    return report_file
+
 
 if __name__ == '__main__':
     # 第一步：加载用例
     cases = add_case()
     # 第二步：执行用例并生成HTML报告
     run_case(cases)
+    # 第三步：获取最新的测试报告
+    report_file = get_report_file(REPORT_PATH)
+    # 第四步：发送邮件
+    Email(title='百度搜素测试报告',
+          message='这是今天的测试报告，请查收！',
+          receiver='收件方邮箱',
+          server='smtp.qq.com',
+          sender='发送方邮箱',
+          password='发送方鉴权码',
+          path=report_file
+          ).send()
